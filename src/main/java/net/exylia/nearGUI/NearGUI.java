bea26dbb210df8baf5e34a871f41560994196d1c
Exylia Services <60438920+DiGround-s@users.noreply.github.com>
2025-05-12 13:24:17 -0400

    Initial commit

---
+++ src/main/java/net/exylia/nearGUI/NearGUI.java
@@ -0,0 +1,35 @@
+package net.exylia.nearGUI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NearGUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+    private void registerCommands() {
+
+    }
+
+    private void loadManagers() {
+
+    }
+
+    private void registerListeners() {
+
+    }
+
+    private void registerConfig() {
+
+    }
+}
